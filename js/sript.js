89c46c788f88788e4c66df1ceb73e5b5d362ba39
rm95542 <rm95542@fiap.com.br>
2022-05-27 23:00:09 -0300

    aulasexta.

--- js/sript.js
+++ js/sript.js
@@ -1,6 +1,24 @@
-//RECUPERAR O BOTÃO E COLOCAR ELE EM UMA CONSTANTE 
+
+//RECUPERAR O BOTÃO E COLOCAR ELE EM UMA CONSTANTE
 const botaoSubmit = document.getElementById('btnEnviar')
 
-//ATRELAR UM EVENTO AO BOTÃO PARA QUE AO SER DISPARADO O EVENTO CLICK 
-//OS CAMPOS/INPUTS SEJAM VALIDADOS(VERIFICADOS SE ESTÃO VAZIOS)
-botaoSubmit.addEventListener('click', )
+//ATRELAR UM EVENTO AO BOTÃO PARA QUE AO SER DISPARADO O EVENTO CLICK
+// OS CAMPOS/INPUTS SEJAM VALIDADOS(VERIFICADOS SE ESTÃO VAZIOS)
+botaoSubmit.addEventListener("click", ()=>{
+    //RECUPERAR OS INPUTS E REALIZAR A VALIDAÇÃO
+    const campos = document.querySelectorAll("input[type='text'],input[type='email']")
+
+    //CRIAR UMA ESTRUTURA PARA REALIZAR A LEITURA DO ARRAY DE INPUTS
+    for(let x = 0 ; x < campos.length ; x++){
+        //CRIAR UMA ESTRUTURA CONDICIONAL PARA
+        //VALIDAR CADA UM DOS CAMPOS
+        if(campos[x].value == ""){
+            alert("O CAMPO "+ campos[x].placeholder +" não foi preenchido!")
+            return
+        }
+    }
+
+    //REALIZANDO O SUBMIT DO FORM
+    
+})
+
